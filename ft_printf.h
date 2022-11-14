1390c138175193e40ae86f2ce43b240810e8e9af
nammal <anassnammal@gmail.com>
2022-11-14 12:18:34 +0100

    lundi 14

--- ft_printf.h
+++ ft_printf.h
@@ -7,16 +7,30 @@
 
 typedef struct s_container
 {
-    int s;
-    int w;
-    int p;
-    int c;
-	int h;
-	int m;
-	char t;
-    char pr;
-    char *content;
+    int hashtag;
+    int minus;
+    int plus;
+    int zero;
+    int space;
+    int width;
+    int precision;
+	char *content;
+	char *conversion;
 } t_con;
+
+
+// typedef struct s_container
+// {
+//     int s;
+//     int w;
+//     int p;
+//     int c;
+// 	int h;
+// 	int m;
+// 	char t;
+//     char pr;
+//     char *content;
+// } t_con;
 
 
 # define BIGGER(a, b) a > b ? a : b
